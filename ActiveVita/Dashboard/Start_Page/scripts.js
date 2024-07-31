e2995a7d64eadeda64e453e88d1ffe3b98ff519b
Saksham Narang <138986069+Saksham-Narang@users.noreply.github.com>
2024-07-31 23:02:43 +0530

    Add files via upload

---
+++ ActiveVita/Dashboard/Start_Page/scripts.js
@@ -0,0 +1,329 @@
+const languageData = {
+    Hindi: {
+        title: "आप कहाँ से हैं?",
+        placeholder: "देहरादून",
+        labels: [
+            "हिंदी (हिंदी)",
+            "अंग्रेज़ी",
+            "तमिल (தமிழ்)",
+            "तेलुगू (తెలుగు)",
+            "कन्नड़ (ಕನ್ನಡ)",
+            "मलयालम (മലയാളം)",
+            "गुजराती (ગુજરાતી)",
+            "बंगाली (বাংলা)",
+            "मराठी (मराठी)",
+            "अन्य"
+        ],
+        skip: "छोड़ें",
+        next: "अगला"
+    },
+    English: {
+        title: "Where are you from?",
+        placeholder: "Dehradun",
+        labels: [
+            "Hindi (हिंदी)",
+            "English",
+            "Tamil (தமிழ்)",
+            "Telugu (తెలుగు)",
+            "Kannada (ಕನ್ನಡ)",
+            "Malayalam (മലയാളം)",
+            "Gujarati (ગુજરાતી)",
+            "Bengali (বাংলা)",
+            "Marathi (मराठी)",
+            "Other"
+        ],
+        skip: "SKIP",
+        next: "NEXT"
+    },
+    Tamil: {
+        title: "நீங்கள் எங்கு இருந்து வருகிறீர்கள்?",
+        placeholder: "தேகராது",
+        labels: [
+            "ஹிந்தி (हिंदी)",
+            "ஆங்கிலம்",
+            "தமிழ் (தமிழ்)",
+            "తెలుగు (తెలుగు)",
+            "ಕನ್ನಡ (ಕನ್ನಡ)",
+            "മലയാളം (മലയാളം)",
+            "ગુજરાતી (ગુજરાતી)",
+            "বাংলা (বাংলা)",
+            "मराठी (मराठी)",
+            "மற்றும்"
+        ],
+        skip: "விரைவில் கடந்து விடு",
+        next: "அடுத்தது"
+    },
+    Telugu: {
+        title: "మీరు ఎక్కడి నుండి వచ్చారు?",
+        placeholder: "దేహరాదున్",
+        labels: [
+            "హిందీ (हिंदी)",
+            "ఆంగ్లం",
+            "తమిళం (தமிழ்)",
+            "తెలుగు (తెలుగు)",
+            "కన్నడ (ಕನ್ನಡ)",
+            "മലയാളം (മലയാളം)",
+            "ગુજરાતી (ગુજરાતી)",
+            "బెంగాలీ (বাংলা)",
+            "मराठी (मराठी)",
+            "ఇతర"
+        ],
+        skip: "వదలడం",
+        next: "తదుపరి"
+    },
+    Kannada: {
+        title: "ನೀವು ಎಲ್ಲಿಂದ ಬರುವಿರಿ?",
+        placeholder: "ದેહ್ರಾದೂನ್",
+        labels: [
+            "ಹಿಂदी (हिंदी)",
+            "ಇಂಗ್ಲಿಷ್",
+            "தமிழ் (தமிழ்)",
+            "తెలుగు (తెలుగు)",
+            "ಕನ್ನಡ (ಕನ್ನಡ)",
+            "മലയാളം (മലയാളം)",
+            "ગુજરાતી (ગુજરાતી)",
+            "ಬಂಗಾಳಿ (বাংলা)",
+            "मराठी (मराठी)",
+            "ಇತರ"
+        ],
+        skip: "ಬಿಟ್ಟುಹೋಗಿ",
+        next: "ಮರು"
+    },
+    Malayalam: {
+        title: "നിങ്ങൾ എവിടെ നിന്നാണ്?",
+        placeholder: "ദേഹ്രദൂൺ",
+        labels: [
+            "ഹിന്ദി (हिंदी)",
+            "ഇംഗ്ലീഷ്",
+            "தமிழ் (தமிழ்)",
+            "తెలుగు (తెలుగు)",
+            "ಕನ್ನಡ (ಕನ್ನಡ)",
+            "മലയാളം (മലയാളം)",
+            "ગુજરાતી (ગુજરાતી)",
+            "বাংলা (বাংলা)",
+            "मराठी (मराठी)",
+            "മറ്റ്"
+        ],
+        skip: "വിട്ടുപോകുക",
+        next: "അടുത്തത്"
+    },
+    Gujarati: {
+        title: "તમે ક્યાંથી છો?",
+        placeholder: "દેહરાદૂન",
+        labels: [
+            "હિન્દી (हिंदी)",
+            "અંગ્રેજી",
+            "தமிழ் (தமிழ்)",
+            "తెలుగు (తెలుగు)",
+            "ಕನ್ನಡ (ಕನ್ನಡ)",
+            "മലയാളം (മലയാളം)",
+            "ગુજરાતી (ગુજરાતી)",
+            "বাংলা (বাংলা)",
+            "मराठी (मराठी)",
+            "અન્ય"
+        ],
+        skip: "છોડી દો",
+        next: "આગળ"
+    },
+    Bengali: {
+        title: "আপনি কোথা থেকে এসেছেন?",
+        placeholder: "দেহরাদুন",
+        labels: [
+            "হিন্দি (हिंदी)",
+            "ইংরেজি",
+            "தமிழ் (தமிழ்)",
+            "తెలుగు (తెలుగు)",
+            "ಕನ್ನಡ (ಕನ್ನಡ)",
+            "മലയാളം (മലയാളം)",
+            "ગુજરાતી (ગુજરાતી)",
+            "বাংলা (বাংলা)",
+            "मराठी (मराठी)",
+            "অন্যান্য"
+        ],
+        skip: "বিলম্ব",
+        next: "পরবর্তী"
+    },
+    Marathi: {
+        title: "तुम्ही कुठून आला आहात?",
+        placeholder: "देहरादून",
+        labels: [
+            "हिंदी (हिंदी)",
+            "इंग्रजी",
+            "தமிழ் (தமிழ்)",
+            "తెలుగు (తెలుగు)",
+            "ಕನ್ನಡ (ಕನ್ನಡ)",
+            "മലയാളം (മലയാളം)",
+            "ગુજરાતી (ગુજરાતી)",
+            "বাংলা (বাংলা)",
+            "मराठी (मराठी)",
+            "इतर"
+        ],
+        skip: "त्याग करा",
+        next: "पुढील"
+    },
+    Other: {
+        title: "Where are you from?",
+        placeholder: "Dehradun",
+        labels: [
+            "Hindi (हिंदी)",
+            "English",
+            "Tamil (தமிழ்)",
+            "Telugu (తెలుగు)",
+            "Kannada (ಕನ್ನಡ)",
+            "Malayalam (മലയാളം)",
+            "Gujarati (ગુજરાતી)",
+            "Bengali (বাংলা)",
+            "Marathi (मराठी)",
+            "Other"
+        ],
+        skip: "SKIP",
+        next: "NEXT"
+    },
+    Spanish: {
+        title: "¿De dónde eres?",
+        placeholder: "Dehradun",
+        labels: [
+            "Hindi (हिंदी)",
+            "Inglés",
+            "Tamil (தமிழ்)",
+            "Telugu (తెలుగు)",
+            "Kannada (ಕನ್ನಡ)",
+            "Malayalam (മലയാളം)",
+            "Gujarati (ગુજરાતી)",
+            "Bengalí (বাংলা)",
+            "Marathi (मराठी)",
+            "Otro"
+        ],
+        skip: "OMITIR",
+        next: "SIGUIENTE"
+    },
+    French: {
+        title: "D'où venez-vous?",
+        placeholder: "Dehradun",
+        labels: [
+            "Hindi (हिंदी)",
+            "Anglais",
+            "Tamoul (தமிழ்)",
+            "Télougou (తెలుగు)",
+            "Kannada (ಕನ್ನಡ)",
+            "Malayalam (മലയാളം)",
+            "Gujarati (ગુજરાતી)",
+            "Bengali (বাংলা)",
+            "Marathi (मराठी)",
+            "Autre"
+        ],
+        skip: "PASSER",
+        next: "SUIVANT"
+    },
+    German: {
+        title: "Woher kommst du?",
+        placeholder: "Dehradun",
+        labels: [
+            "Hindi (हिंदी)",
+            "Englisch",
+            "Tamil (தமிழ்)",
+            "Telugu (తెలుగు)",
+            "Kannada (ಕನ್ನಡ)",
+            "Malayalam (മലയാളം)",
+            "Gujarati (ગુજરાતી)",
+            "Bengali (বাংলা)",
+            "Marathi (मराठी)",
+            "Andere"
+        ],
+        skip: "ÜBERSPRINGEN",
+        next: "WEITER"
+    },
+    Chinese: {
+        title: "你来自哪里？",
+        placeholder: "德拉敦",
+        labels: [
+            "印地语 (हिंदी)",
+            "英语",
+            "泰米尔语 (தமிழ்)",
+            "泰卢固语 (తెలుగు)",
+            "坎纳达语 (ಕನ್ನಡ)",
+            "马拉雅拉姆语 (മലയാളം)",
+            "古吉拉特语 (ગુજરાતી)",
+            "孟加拉语 (বাংলা)",
+            "马拉地语 (मराठी)",
+            "其他"
+        ],
+        skip: "跳过",
+        next: "下一步"
+    }
+};
+
+
+// Function to change the language
+function changeLanguage(lang) {
+    const data = languageData[lang];
+    
+    if (data) {
+        document.querySelector('h1').textContent = data.title;
+        document.querySelector('input[type="text"]').placeholder = data.placeholder;
+        
+        const labels = document.querySelectorAll('.radio-group label');
+        labels.forEach((label, index) => {
+            const input = label.querySelector('input');
+            if (input) {
+                label.textContent = data.labels[index];
+                label.prepend(input);
+            }
+        });
+        
+        document.querySelector('.skip-button').textContent = data.skip;
+        document.querySelector('button[type="button"]').textContent = data.next;
+    }
+}
+
+// Event listener for language change
+document.querySelectorAll('.radio-group input').forEach(input => {
+    input.addEventListener('change', (event) => {
+        const selectedLanguage = event.target.value;
+        changeLanguage(selectedLanguage);
+    });
+});
+
+// Page Navigation Functions
+function nextStep() {
+    const currentPage = getCurrentPage();
+    const nextPage = getNextPage(currentPage);
+    window.location.href = nextPage;
+}
+
+function getCurrentPage() {
+    const currentPath = window.location.pathname;
+    const currentPage = currentPath.substring(currentPath.lastIndexOf('/') + 1);
+    return currentPage;
+}
+
+function getNextPage(currentPage) {
+    switch (currentPage) {
+        case 'index.html':
+            return 'age.html';
+        case 'age.html':
+            return 'wherefrom.html';
+        case 'wherefrom.html':
+            return 'height.html';
+        case 'height.html':
+            return 'currentweight.html';
+        case 'currentweight.html':
+            return 'targetweight.html';
+        case 'targetweight.html':
+            return 'goal.html';
+        case 'goal.html':
+            return 'medical.html';
+        default:
+            return 'index.html';
+    }
+}
+
+function goBack() {
+    window.history.back();
+}
+
+function skip() {
+    const currentPage = getCurrentPage();
+    const nextPage = getNextPage(currentPage);
+    window.location.href = nextPage;
+}
---
+++ ActiveVita/Dashboard/order.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Monday',
+        productNumber: 'Chest',
+        paymentStatus: '',
+        status: ''
+    },
+    {
+        productName: 'Tuesday',
+        productNumber: 'Back',
+        paymentStatus: '',
+        status: ''
+    },
+    {
+        productName: 'Wednesday',
+        productNumber: 'Legs',
+        paymentStatus: '',
+        status: ''
+    },
+    {
+        productName: 'Thursady',
+        productNumber: 'Arms',
+        paymentStatus: '',
+        status: ''
+    },
+    {
+        productName: 'Friday',
+        productNumber: 'Delts',
+        paymentStatus: '',
+        status: ''
+    },
+    {
+        productName: 'Saturday',
+        productNumber: 'Abs',
+        paymentStatus: '',
+        status: ''
+    },
+    {
+        productName: 'Sunday',
+        productNumber: 'Rest',
+        paymentStatus: '',
+        status: ''
+    },
+]
